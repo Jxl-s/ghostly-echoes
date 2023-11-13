9ec2959a1eeaaeb15e4cf1bff7afcbf66c9ab3ff
DokuSan00 <kanekike12@gmail.com>
2023-11-12 21:20:25 -0500

    added Lerping effect. InHandCam now rotate with Cam

--- Assets/Scripts/CameraRotation.cs
+++ Assets/Scripts/CameraRotation.cs
@@ -11,21 +11,30 @@ public class CameraRotation : MonoBehaviour
     }
 
     float sensitivity = 1f;
-    float yRotationLimit = 88f;
+    public float smoothTurnRate = 0.15f; //0 - 1 || 1 -> instant
+    // since this is camera movement, -> higher than 0.3 is already not noticable
+
+    float yRotationLimit = 75f;
 
     Vector2 rotation = Vector2.zero;
     const string xAxis = "Mouse X"; //Strings in direct code generate garbage, storing and re-using them creates no garbage
     const string yAxis = "Mouse Y";
+
+    public GameObject InHandObject;
 
     void Update()
     {
         rotation.x += Input.GetAxis(xAxis) * sensitivity;
         rotation.y += Input.GetAxis(yAxis) * sensitivity;
         rotation.y = Mathf.Clamp(rotation.y, -yRotationLimit, yRotationLimit);
+
         var xQuat = Quaternion.AngleAxis(rotation.x, Vector3.up);
         var yQuat = Quaternion.AngleAxis(rotation.y, Vector3.left);
+        
 
-        transform.parent.transform.localRotation = xQuat;
-        transform.localRotation = yQuat; //Quaternions seem to rotate more consistently than EulerAngles. Sensitivity seemed to change slightly at certain degrees using Euler. transform.localEulerAngles = new Vector3(-rotation.y, rotation.x, 0);
+        transform.parent.transform.localRotation = Quaternion.Slerp(transform.parent.transform.localRotation, xQuat, smoothTurnRate);
+
+        transform.localRotation = Quaternion.Slerp(transform.localRotation, yQuat, smoothTurnRate);
+        InHandObject.transform.localRotation = transform.localRotation;
     }
 }
