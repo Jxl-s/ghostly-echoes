34bf7835e7319faef78c1800d942aacd4a1f1782
j <77560326+Jxl-s@users.noreply.github.com>
2023-11-21 00:56:42 -0500

    Add ceiling light prefab with random delays

---
+++ Assets/Scripts/CeilingLightBehaviour.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CeilingLightBehaviour : MonoBehaviour
+{
+    public float minFlashInterval = 4f;
+    public float maxFlashInterval = 10f;
+    public float minFlashDuration = 0.1f;
+    public float maxFlashDuration = 1f;
+
+    public Light lightComponent;
+    private float delay;
+
+    void Start()
+    {
+        // make a random delay
+        delay = Random.Range(0f, maxFlashInterval);
+
+        // Start the flashing coroutine
+        StartCoroutine(FlashLight());
+    }
+
+    IEnumerator FlashLight()
+    {
+        // Wait for the delay
+        yield return new WaitForSeconds(delay);
+
+        while (true)
+        {
+            // Turn on the light
+            lightComponent.enabled = false;
+
+            // Wait for the duration of the flash
+            float randomDuration = Random.Range(minFlashDuration, maxFlashDuration);
+            yield return new WaitForSeconds(randomDuration);
+
+            // Turn off the light
+            lightComponent.enabled = true;
+
+            // Wait for a random interval before the next flash
+            float randomInterval = Random.Range(minFlashInterval, maxFlashInterval);
+            yield return new WaitForSeconds(randomInterval);
+        }
+    }
+}
