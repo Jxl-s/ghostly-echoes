431192a2d3b1dd51e2236ce634b574a907c50c4f
DokuSan00 <kanekike12@gmail.com>
2023-11-23 07:19:29 -0500

    finished first floor decoration

--- Assets/Scripts/LightScript.cs
+++ Assets/Scripts/LightScript.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
-using UnityEditor.UI;
+// using UnityEditor.UI;
 using UnityEngine;
 using UnityEngine.Experimental.GlobalIllumination;
 using UnityEngine.UI;
